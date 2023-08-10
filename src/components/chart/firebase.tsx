27af6da2dc7020ac5b083a01fef2ffa6a5063671
Kem Guianalan <guianalankem@gmail.com>
2023-08-11 01:06:21 +0800

    Update firebase.tsx
    remove private variables.

--- src/components/chart/firebase.tsx
+++ src/components/chart/firebase.tsx
@@ -2,15 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDKDx2bB7_7KfXyOJ2qj1kxQYi-fVpTdvQ",
-    authDomain: "humidtytemperaturemonitoring.firebaseapp.com",
-    databaseURL: "https://humidtytemperaturemonitoring-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "humidtytemperaturemonitoring",
-    storageBucket: "humidtytemperaturemonitoring.appspot.com",
-    messagingSenderId: "400924063627",
-    appId: "1:400924063627:web:044db69712362b221b618e",
-    measurementId: "G-WM09L81C4S",
-    userId: "18Dli3WGbKa79i1bhQPjMw3irSl2"
+    apiKey: "Change to your API",
+    authDomain: "Change to your authDomain",
+    databaseURL: "Change to your databaseURL",
+    projectId: "Change to your projectId",
+    storageBucket: "Change to your storageBucket",
+    messagingSenderId: "Change to your messagingSenderId",
+    appId: "Change to your appId",
+    measurementId: "Change to your measurementId",
+    userId: "Change to your userId"
 };
 
 export const app = initializeApp(firebaseConfig);
